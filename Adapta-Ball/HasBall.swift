615c7a0fa1b236dfe9dffdb633c82fccf54ae77a
Anusha Kankipati <anushakankipati@anushas-macbook-air.local>
2020-04-13 00:56:09 -0700

    3rd page

---
+++ Adapta-Ball/HasBall.swift
@@ -0,0 +1,31 @@
+//
+//  HasBall.swift
+//  Adapta-Ball
+//
+//  Created by Anusha Kankipati on 4/13/20.
+//  Copyright © 2020 sea urchins. All rights reserved.
+//
+
+import UIKit
+
+class HasBall: UIViewController {
+
+    @IBOutlet weak var hasBallTitle: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
