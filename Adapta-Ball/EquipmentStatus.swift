f961f81c29fa9d76afa564a6905b762caca690c0
Anusha Kankipati <anushakankipati@anushas-macbook-air.local>
2020-04-12 20:05:05 -0700

    first push

---
+++ Adapta-Ball/EquipmentStatus.swift
@@ -0,0 +1,30 @@
+//
+//  EquipmentStatus.swift
+//  Adapta-Ball
+//
+//  Created by Anusha Kankipati on 4/12/20.
+//  Copyright © 2020 sea urchins. All rights reserved.
+//
+
+import UIKit
+
+class EquipmentStatus: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
